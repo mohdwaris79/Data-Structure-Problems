5c73d1a16d1e4bf4611c5aad6292197938c6ed52
mohdwaris79 <mohdwaris7892@gmail.com>
2025-08-25 13:03:35 +0530

    Add LongestIncreasingSubsequence solutions

---
+++ LongestIncreasingSubsequence.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class LongestIncreasingSubsequence {
+    
+    public static int LongestIncreasingSubsequence(int[] arr) {
+        int n = arr.length;
+        int[] dp = new int[n];      //first created a dp array
+        Arrays.fill(dp, 1);
+
+        int maxLen = 1;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLen = Math.max(maxLen, dp[i]);
+        }
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
+        System.out.println(LongestIncreasingSubsequence(nums)); // Output: 4
+    }
+}
