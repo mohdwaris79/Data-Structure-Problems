238b5b3189fd3eadce465ea39fb4f550511dc381
mohdwaris79 <mohdwaris7892@gmail.com>
2025-09-02 22:58:58 +0530

    Anagram string solution

---
+++ Array/Anagram.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Anagram{
+    public static boolean anagram(String str1, String str2)
+    {
+        if(str1.length()!=str2.length())
+        {
+            return false;
+        }
+
+        char []arr1=str1.toCharArray();
+        char []arr2=str2.toCharArray();
+
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+
+        return Arrays.equals(arr1,arr2);
+    }
+    public static void main(String args[])
+    {
+        String str1="hello";
+        String str2="oellh";
+        System.out.println(anagram(str1,str2)?"Anagram":"Not Anagram");
+
+
+    }
+}
