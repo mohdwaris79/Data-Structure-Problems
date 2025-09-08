941839a398c75291b097bf9b512d4c8b88907b3d
mohdwaris79 <mohdwaris7892@gmail.com>
2025-09-08 12:49:23 +0530

    SubArray Sum of Size K

---
+++ Array/SubarraySumOfSizeK.java
@@ -0,0 +1,26 @@
+public class SubarraySumOfSizeK {
+    public static int SubArray(int arr[], int k)
+    {
+        int n=arr.length;
+        int maxSum=0;
+        int currSum=0;
+        for(int i =0;i<k;i++)
+        {
+            maxSum+=arr[i];
+        }
+        currSum=maxSum;
+        for(int i=k;i<n;i++)
+        {
+            currSum+=arr[i]-arr[i-k];
+            maxSum=Math.max(maxSum,currSum);
+        }
+
+        return maxSum;
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6,8,9,15};
+        int k=3;
+        System.out.println(SubArray(arr, k));
+    }
+    
+}
