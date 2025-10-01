3f8365ff4e1915c88317b6a2affbdc7e0d50ff5b
mohdwaris79 <mohdwaris7892@gmail.com>
2025-10-01 17:31:09 +0530

    count Nodes of Tree

---
+++ Tree/CountNode.java
@@ -0,0 +1,31 @@
+class Node{
+    int val;
+    Node left;
+    Node right;
+    Node(int val)
+    {
+        this.val=val;
+        left=right=null;
+    }
+}
+public class CountNode {
+    public static int count(Node root)
+    {
+        if(root==null)
+        {
+            return 0;
+        }
+        return count(root.left)+count(root.right)+1;
+    }
+    public static void main(String[] args) {
+        Node root=new Node(1);
+        root.left=new Node(2);
+        root.right=new Node(3);
+        root.left.left=new Node(4);
+        root.left.right=new Node(5);
+        root.right.left=new Node(6);
+        root.right.right=new Node(7);
+        System.out.println(count(root));
+    }
+    
+}
