206dabc99f1e9d2388225fb160896ffa97ab6f61
mohdwaris79 <mohdwaris7892@gmail.com>
2025-10-16 23:44:42 +0530

    Palindrome checker

---
+++ String/Palindrome.java
@@ -0,0 +1,12 @@
+class Palindrome {
+    public static void main(String[] args) {
+        String str = "madam";
+        String reversed = new StringBuilder(str).reverse().toString();
+
+        if (str.equals(reversed)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
