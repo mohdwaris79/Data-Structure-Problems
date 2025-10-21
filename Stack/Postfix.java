a1a879ef463da0c47008e1ce97a522ff12567342
mohdwaris79 <mohdwaris7892@gmail.com>
2025-10-21 21:20:35 +0530

    Postfix Expression

---
+++ Stack/Postfix.java
@@ -0,0 +1,30 @@
+
+    
+
+import java.util.*;
+
+public class Postfix {
+    public static int evaluate(String expr) {
+        Stack<Integer> stack = new Stack<>();
+        for (char ch : expr.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                stack.push(ch - '0');
+            } else {
+                int val2 = stack.pop();
+                int val1 = stack.pop();
+                switch (ch) {
+                    case '+': stack.push(val1 + val2); break;
+                    case '-': stack.push(val1 - val2); break;
+                    case '*': stack.push(val1 * val2); break;
+                    case '/': stack.push(val1 / val2); break;
+                }
+            }
+        }
+        return stack.pop();
+    }
+
+    public static void main(String[] args) {
+        String expr = "231*+9-";
+        System.out.println("Result: " + evaluate(expr)); // Output: -4
+    }
+}
