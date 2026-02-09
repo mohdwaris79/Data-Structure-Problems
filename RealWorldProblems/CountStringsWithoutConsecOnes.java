d39e1e9c6a0798022fddbbcaaef25cfa3cb87201
mohdwaris79 <mohdwaris7892@gmail.com>
2026-02-09 14:35:20 +0530

    Count Binary Strings Without Consecutive 1's

--- RealWorldProblems/CountStringsWithoutConsecOnes.java
+++ RealWorldProblems/CountStringsWithoutConsecOnes.java
@@ -4,12 +4,12 @@ public class CountStringsWithoutConsecOnes {
     public int countBinaryStrings(int n) {
         if (n == 1) return 2;
 
-        int dp0 = 1;
-        int dp1 = 1;
+        int dp0 = 1;                   // dp0 for the zero integer
+        int dp1 = 1;                   // dp1 for the one
 
         for (int i = 2; i <= n; i++) {
-            int newDp0 = dp0 + dp1;
-            int newDp1 = dp0;
+            int newDp0 = dp0 + dp1;         // if the last index is zero then we can add anything after zero
+            int newDp1 = dp0;               // if the last indext is one so we only can add anything after zero
 
             dp0 = newDp0;
             dp1 = newDp1;
