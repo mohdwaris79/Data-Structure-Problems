891776a9cf8aff0376f813ba418cf44f3e2f496d
mohdwaris79 <mohdwaris7892@gmail.com>
2026-02-20 12:13:55 +0530

    Add another Approach

--- Array/BuyAndSellStocks.java
+++ Array/BuyAndSellStocks.java
@@ -1,6 +1,8 @@
 public class BuyAndSellStocks {
     public static int bss(int price[])
     {
+
+        // Approach 1
         int buyPrice=Integer.MAX_VALUE;
         int maxProfit=0;
         for(int i=0;i<price.length; i++)
@@ -16,6 +18,26 @@ public class BuyAndSellStocks {
         }
         return maxProfit;
     }
+
+
+
+    // Approach 2
+
+    // int minPrice=Integer.MAX_VALUE;
+    // int maxProfit=0;
+    // for(int i=0;i<price.length;i++)
+    // {
+    //     if(buyPrice<price)
+    //     {
+    //         maxProfit=Math.max(maxprofit, buyPrice-price);
+    //         minPrice=Math.min(minprice, price);
+    //     }
+    //     return maxProfit;
+    // }
+
+
+
+    
     public static void main(String[] args) {
         int price[]={40,50,500,700,100,2000,6000,8000};
         System.out.println(bss(price));
